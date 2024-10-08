b4ec448d14639f530f42aabfa55b2a6033cbc5d4
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-10-08 21:04:41 +0000

    update wrap up form submission

--- public/javascripts/monthly-wrap-up-form.js
+++ public/javascripts/monthly-wrap-up-form.js
@@ -13,13 +13,92 @@ const initMonthlyWrapUp = (() => {
     license_key: "gpl",
   });
 
-  const form = document.querySelector("#wrap-up-form");
+  const form = document.getElementById("wrap-up-form");
   const summaryInput = document.getElementById("summary");
+  const messageDiv = document.getElementById("message");
 
-  form.onsubmit = (e) => {
+  form.onsubmit = async (e) => {
     e.preventDefault();
-    summaryInput.value = tinymce.activeEditor.getContent();
-    console.log(summaryInput.value);
-    form.submit();
+
+    try {
+      summaryInput.value = tinymce.activeEditor.getContent();
+      console.log(summaryInput.value);
+      let data = {
+        month: form.elements["month"].value,
+        year: form.elements["year"].value,
+        summary: tinymce.activeEditor.getContent(),
+      };
+
+      const response = await postData(document.URL, data);
+      const result = await response.json();
+      if (response.ok) {
+        displayMessage("success", result);
+      } else {
+        displayMessage("error", result);
+      }
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    });
+    return response;
+  }
+
+  function displayMessage(type, result) {
+    form.style.pointerEvents = "none";
+    form.style.opacity = 0.5;
+
+    if (type === "success") {
+      messageDiv.innerHTML = "";
+      let p = document.createElement("p");
+      p.innerHTML = "Review successfully saved. ";
+
+      let a = document.createElement("a");
+      a.href = result.url;
+      a.innerHTML = "View review.";
+      p.append(a);
+
+      const button = document.createElement("button");
+      button.innerHTML = "x";
+      button.onclick = closeMessage;
+
+      messageDiv.append(p, button);
+      messageDiv.classList.add("pop-up");
+    } else {
+      messageDiv.innerHTML = "";
+      let ul = document.createElement("ul");
+      let p = (document.createElement("p").innerHTML = "Error(s) saving book:");
+      for (const error of result) {
+        let li = document.createElement("li");
+        li.innerHTML = error.msg;
+        ul.append(li);
+      }
+
+      const button = document.createElement("button");
+      button.innerHTML = "x";
+      button.onclick = closeMessage;
+
+      messageDiv.append(button, p, ul);
+
+      messageDiv.classList.add("pop-up");
+    }
+
+    function closeMessage() {
+      messageDiv.classList.remove("pop-up");
+      messageDiv.innerHTML = "";
+      form.style.pointerEvents = "auto";
+      form.style.opacity = 1;
+    }
+  }
 })();
